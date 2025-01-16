e74b6f3b31fb9ed4ed5910613341bde2005823ad
xtianbong <christianmbong45@gmail.com>
2025-01-16 12:54:53 -0500

    trying to change the button text

--- assets/force-size-selector.js
+++ assets/force-size-selector.js
@@ -1,3 +1,11 @@
 //Disables the add to cart button until the customer picks a size
 
 console.log("Force size working");
+
+const addToCartButton = document.querySelector(".button__add-to-cart");
+
+function forceSize(){
+    addToCartButton.innerHTML = "<span data-add-to-cart-text="">Select a size</span>"
+}
+
+forceSize()
