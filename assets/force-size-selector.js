8b064058f97ee4b5a061b337713302d48cafd784
xtianbong <christianmbong45@gmail.com>
2025-01-16 13:18:56 -0500

    re

--- assets/force-size-selector.js
+++ assets/force-size-selector.js
@@ -4,8 +4,28 @@ console.log("Force size working");
 
 const addToCartButton = document.querySelector(".button__add-to-cart");
 
+const sizeSelectorDivs = document.querySelector(".form__field--swatches");
+
+console.log(sizeSelectorDivs);
+
+var addToCartButtonHtml = ''
+
+//force the customer to select a size when the page loads
 function forceSize(){
+    //store the innerhtml of the button before replacing it
+    addToCartButtonHtml = addToCartButton.innerHTML;
     addToCartButton.innerHTML = '<span data-add-to-cart-text="">Select a size</span>';
 }
+
+
+//once they select a size let them add the item to their cart
+function enableButton(){
+    if(addToCartButtonHtml != ''){
+        addToCartButton.innerHTML = addToCartButtonHtml
+    }
+    
+}
+
+
 
 forceSize()
